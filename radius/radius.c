78b054f53a598b7efa1e1f69e8537fef60cb0050
georemo <george.oremo@gmail.com>
2023-12-19 01:31:59 +0300

    -

---
+++ radius/radius.c
@@ -0,0 +1,16 @@
+/**
+ * Sams Teach Yourself C in 21 Days,
+ * 6xth Edition
+ * pg 22
+ */
+
+#include <stdio.h>
+int radius, area;
+int main(void)
+{
+    printf("Enter radius (i.e. 10): ");
+    scanf("%d", &radius);
+    area = (int)(3.14159 * radius * radius);
+    printf("\n\nArea = %d\n", area);
+    return 0;
+}
